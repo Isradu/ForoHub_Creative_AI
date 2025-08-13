26fb9591ec03dfb0cfd95ee0935721c164f28fab
Israel Durán <duranjimenezisrael@gmail.com>
2025-08-13 16:21:49 -0600

    feat: Configuración inicial del proyecto con Spring Initializr y las dependencias.

---
+++ ForoHub_Creative_AI/src/main/java/com/forohub/forohub_creative_ai/ForoHubCreativeAiApplication.java
@@ -0,0 +1,13 @@
+package com.forohub.forohub_creative_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubCreativeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForoHubCreativeAiApplication.class, args);
+    }
+
+}
---
+++ ForoHub_Creative_AI/src/test/java/com/forohub/forohub_creative_ai/ForoHubCreativeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.forohub_creative_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubCreativeAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
